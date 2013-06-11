9dd10339885e40c11f7c4cca814bf2816dc9e84c
Dan Costinett <dcostinett@llnw.com>
2013-06-10 17:46:30 -0700

    add invariant check

--- src/edu/uw/danco/remote/RemoteBrokerSessionImpl.java
+++ src/edu/uw/danco/remote/RemoteBrokerSessionImpl.java
@@ -54,6 +54,7 @@ public class RemoteBrokerSessionImpl extends UnicastRemoteObject implements Remo
     @Override
     public void deleteAccount() throws RemoteException {
         try {
+            checkInvariants();
             LOGGER.info("Deleting account: " + account.getName());
             broker.deleteAccount(account.getName());
         } catch (BrokerException e) {
@@ -71,6 +72,7 @@ public class RemoteBrokerSessionImpl extends UnicastRemoteObject implements Remo
     @Override
     public StockQuote requestQuote(String ticker) throws RemoteException {
         try {
+            checkInvariants();
             LOGGER.info("Quote requested for: " + ticker);
             return broker.requestQuote(ticker);
         } catch (BrokerException e) {
@@ -89,6 +91,7 @@ public class RemoteBrokerSessionImpl extends UnicastRemoteObject implements Remo
     public void placeMarketBuyOrder(final int numberOfShares, final String ticker)
             throws RemoteException {
         try {
+            checkInvariants();
             final MarketBuyOrder order = new MarketBuyOrder(account.getName(), numberOfShares, ticker);
             LOGGER.info("Placing MarketBuyOrder: " + order);
             broker.placeOrder(order);
@@ -108,6 +111,7 @@ public class RemoteBrokerSessionImpl extends UnicastRemoteObject implements Remo
     public void placeMarketSellOrder(final int numberOfShares, final String ticker)
             throws RemoteException {
         try {
+            checkInvariants();
             final MarketSellOrder order = new MarketSellOrder(account.getName(), numberOfShares, ticker);
             LOGGER.info("Placing MarketSellOrder: " + order);
             broker.placeOrder(order);
@@ -129,6 +133,7 @@ public class RemoteBrokerSessionImpl extends UnicastRemoteObject implements Remo
                                   final String ticker,
                                   final int stopPrice) throws RemoteException {
         try {
+            checkInvariants();
             final StopBuyOrder order =
                     new StopBuyOrder(account.getName(), numberOfShares, ticker, stopPrice);
             LOGGER.info("Placing StopBuyOrder: " + order);
@@ -151,6 +156,7 @@ public class RemoteBrokerSessionImpl extends UnicastRemoteObject implements Remo
                                    final String ticker,
                                    final int stopPrice) throws RemoteException {
         try {
+            checkInvariants();
             final StopSellOrder order =
                     new StopSellOrder(account.getName(), numberOfShares, ticker, stopPrice);
             LOGGER.info("Placing StopSellOrder: " + order);
@@ -162,16 +168,29 @@ public class RemoteBrokerSessionImpl extends UnicastRemoteObject implements Remo
 
 
     /**
-     *
+     * Close the broker
      * @throws RemoteException
      */
     @Override
     public void close() throws RemoteException {
         try {
+            checkInvariants();
             LOGGER.info("Closing the broker.");
             broker.close();
+            account = null;
         } catch (BrokerException e) {
             throw new RemoteException(e.getMessage(), e);
+        }
+    }
+
+
+    /**
+     * Verify object state
+     */
+    private void checkInvariants() {
+        if (account == null ||
+                broker == null ) {
+            throw new IllegalStateException("Broker session is not properly initialized, or has been closed.");
         }
     }
 }
